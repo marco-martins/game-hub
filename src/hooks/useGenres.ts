e264e2ab844e6713c28c27d01c650e9d83068eb1
Marco Martins <marko.double@gmail.com>
2023-09-13 11:06:20 +0100

    Refactor: fetch data with React Query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,8 @@
 // import useData from "./useData";
+
+import { useQuery } from "@tanstack/react-query";
+import apiClient, { FetchResponse } from "../services/api-client";
+
 import genres from "../data/genres";
 
 export interface Genre {
@@ -8,7 +12,18 @@ export interface Genre {
 }
 
 // const useGenres = () => useData<Genre>("/genres");
+
 // using local data insted
-const useGenres = () => ({ data: genres, isLoading: false, error: null });
+// const useGenres = () => ({ data: genres, isLoading: false, error: null });
+
+// using react query
+const useGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24h cache
+    initialData: { count: genres.length, results: genres },
+  });
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,7 @@
 // import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import apiClient, { FetchResponse } from "../services/api-client";
 
 export interface Platform {
   id: number;
@@ -9,6 +11,18 @@ export interface Platform {
 
 // const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
 // using local data insted
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+// const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+
+// using react query
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/list/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24h cache
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatforms;
